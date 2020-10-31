6ee4839d36e85f1dccab6f718126ec4d54b3b760
Sin Wildy <sw.wildy@gmail.com>
2020-10-31 20:45:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,13 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    programmer_hash[:yukihiro_matsumoto][:known_for] = ["Ruby"]
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
+    
+    
+    [:known_for] = ["Ruby"]
     programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
     programmer_hash
 end
